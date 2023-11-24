c8f22cb705c35d005557480eb2fb51bc2c2633e2
charanreddy404 <charanlucky6143@gmail.com>
2023-11-24 05:44:42 +0530

    Authentication with next-auth

--- next.config.js
+++ next.config.js
@@ -4,10 +4,18 @@ const nextConfig = {
     API_URL: "http://localhost:3000",
     DB_LOCAL_URI: "mongodb://127.0.0.1:27017/bookit-nextjs",
     DB_URI: "",
+    NEXTAUTH_URL: "http://localhost:3000",
+    NEXTAUTH_SECRET: "ASDFGHJKLZXCVBNMQWERTYUIOP",
   },
   reactStrictMode: false,
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
